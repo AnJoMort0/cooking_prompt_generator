308c5c32b72a7757ac5b410be575cbfda2f462a4
AnJoMorto <andrejsfonseca@gmail.com>
2026-09-21 16:31:12 +0100

    Defer select value until options & bump SW version
    Avoid setting a select element's value while iterating props (which can make the browser pick the first option before options exist). Store props locally, skip the "value" during the initial apply loop, then set el.value after option children are appended (with try/catch). Also bump service worker VERSION to "mise-pwa-v11" and update related cache names.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "mise-pwa-v10";
+const VERSION = "mise-pwa-v11";
 const SHELL_CACHE = `${VERSION}-shell`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const LUCIDE_URL = "https://unpkg.com/lucide@1.47.0";
